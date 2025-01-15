61875226c925a5dd3289a96210ab84d91d618928
HyBRiZx420 <mitch.hermanos@web.de>
2025-01-15 16:29:38 +0100

    final_shape
    push

--- data_analytics/MM/slicing.sql
+++ data_analytics/MM/slicing.sql
@@ -1,27 +1,19 @@
-SELECT
-	planet_id,
-    star_id,
-    spectral_type,
-    (COUNT(DISTINCT spectral_type))
-FROM values
-GROUP BY planet_id, star_id, spectral_type;
-
-
-
-
-SELECT
-	DISTINCT s.spectral_type,
-	         s.host_star_name,
-	         s.star_id,
-	         p.planet_id
+SELECT p.planet_name,
+	   v.orbital_period_days,
+	   v.mass_wrt,
+	   v.mass_multiplier,
+	   s.host_star_name,
+	   s.spectral_type
 FROM values AS v
-JOIN stars AS s on s.star_id = v.star_id
-JOIN planets AS p on s.star_id = p.star_id
-ORDER BY spectral_type;
+		 JOIN
+	 stars AS s
+	 ON
+		 v.star_id = s.star_id
+		 JOIN planets AS p on s.star_id = p.star_id
+WHERE v.orbital_period_days IS NOT NULL
+  AND s.spectral_type IS NOT NULL
+  AND p.planet_type = 'Terrestrial'
+  AND v.mass_wrt = 'Earth'
+ORDER BY v.spectral_type;
 
 
-SELECT
-	COUNT(sp)
-	
-	
-FROM terrestrial_planets;
--- usermanagement/role_user_management.sql
+++ usermanagement/role_user_management.sql
@@ -12,15 +12,10 @@ GRANT SELECT ON TABLES TO read_only_role;
 CREATE USER eduard WITH PASSWORD 'eduard123';
 CREATE USER süleyman WITH PASSWORD 'süleyman123';
 CREATE USER nikolay WITH PASSWORD 'nikolay123';
-CREATE USER mrd WITH PASSWORD 'mrd42';
 
 GRANT read_only_role TO eduard;
 GRANT read_only_role TO süleyman;
 GRANT read_only_role TO nikolay;
-GRANT read_only_role TO mrd;
-
-
-
 
 SELECT *
 FROM pg_roles;
